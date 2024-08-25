d325b540d389aee656a4e3c22db1a78b744095ba
Kazuki Shimizu <kazubu@jtime.net>
2024-08-25 14:05:04 +0900

    initial commit

---
+++ etherip-over-ipv6.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# IP network side
+ifconfig em1 up
+ifconfig em1 inet6 2001:db8::1/64
+ifconfig em1 -rxcsum -txcsum -lro -rxcsum6 -txcsum6 up 
+
+# Ethernet side
+ifconfig em2 -rxcsum -txcsum -lro -rxcsum6 -txcsum6 up 
+
+# Create tunnel interface
+ifconfig gif0 create
+ifconfig gif0 inet6 tunnel 2001:db8::1 2001:db8::2
+ifconfig gif0 mtu 1500
+ifconfig gif0 link0
+ifconfig gif0 up
+
+# Create bridge between physical interface and tunnel interface
+ifconfig bridge0 create
+ifconfig bridge0 mtu 1500
+ifconfig bridge0 addm gif0
+ifconfig bridge0 addm em2
+ifconfig bridge0 up
